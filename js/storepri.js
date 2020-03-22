15e2d3af24af54e2cfca71d5c3ad17c1e64fc2ee
chenlhlinux <chenlhlinux@outlook.com>
2020-03-22 11:27:58 +0800

    修复变量作用域的错误

--- js/storepri.js
+++ js/storepri.js
@@ -1,29 +1,36 @@
 function PriSessSave(name,pri){
 	(async () => {
 		if(typeof(Storage)!=="undefined"){
-			if(typeof(sessionStorage.prikeys)!=="undefined"){
-				if(JSON.parse(sessionStorage.prikeys).includes([name,pri])){ //这一段判断是否重复的代码似乎没有正常工作。
-					window.alert("此私钥已保存");
+			try{
+				if(typeof(sessionStorage.prikeys)!=="undefined"){
+					if(JSON.parse(sessionStorage.prikeys).includes([name,pri])){ //这一段判断是否重复的代码似乎没有正常工作。
+						window.alert("此私钥已保存");
+					}else{
+						if(typeof(sessionStorage.prikeysidindex)==="undefined")
+							PriGenIndex();
+						var data=JSON.parse(sessionStorage.prikeys);
+						var keysindex=JSON.parse(sessionStorage.prikeysidindex);
+						var len=data.length;
+						data[len]=[name,pri];
+						var keyidlen=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds().length;
+						var keyid=new Array();
+						for(ii=0;ii<keyidlen;++ii)
+							keyid[ii]=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds()[ii].toHex();
+						keysindex[len]=[keyid,len];
+						sessionStorage.prikeys=JSON.stringify(data);
+						sessionStorage.prikeysidindex=JSON.stringify(keysindex);
+					}
 				}else{
-					data=JSON.parse(sessionStorage.prikeys);
-					keysindex=JSON.parse(localStorage.prikeysidindex);
-					len=data.length;
-					data[len]=[name,pri];
-					keyidlen=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds().length;
-					keyid=new Array();
+					sessionStorage.prikeys=JSON.stringify(new Array([name,pri]));
+					var keyidlen=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds().length;
+					var keyid=new Array();
 					for(ii=0;ii<keyidlen;++ii)
 						keyid[ii]=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds()[ii].toHex();
-					keysindex[len]=[keyid,len];
-					sessionStorage.prikeys=JSON.stringify(data);
-					sessionStorage.prikeysidindex=JSON.stringify(keysindex);
+					sessionStorage.prikeysidindex=JSON.stringify(new Array([keyid,0]));
 				}
-			}else{
-				sessionStorage.prikeys=JSON.stringify(new Array([name,pri]));
-				keyidlen=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds().length;
-				keyid=new Array();
-				for(ii=0;ii<keyidlen;++ii)
-					keyid[ii]=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds()[ii].toHex();
-				sessionStorage.prikeysidindex=JSON.stringify(new Array([keyid,0]));
+			}catch(e){
+				alert("出错：\n调用函数：PriSessSave("+name+','+pri+")\n错误代码："+e);
+				throw new Error(e);
 			}
 		}else{
 			window.alert("您的浏览器不支持此功能，请更新浏览器");
@@ -34,29 +41,36 @@ function PriSessSave(name,pri){
 function  PriLocalSave(name,pri){
 	(async () => {
 		if(typeof(Storage)!=="undefined"){
-			if(typeof(localStorage.prikeys)!=="undefined"){
-				if(JSON.parse(localStorage.prikeys).includes([name,pri])){ //这一段判断是否重复的代码似乎没有正常工作。
-					window.alert("此私钥已保存");
+			try{
+				if(typeof(localStorage.prikeys)!=="undefined"){
+					if(JSON.parse(localStorage.prikeys).includes([name,pri])){ //这一段判断是否重复的代码似乎没有正常工作。
+						window.alert("此私钥已保存");
+					}else{
+						if(typeof(localStorage.prikeysidindex)==="undefined")
+							PriGenIndex();
+						var data=JSON.parse(localStorage.prikeys);
+						var keysindex=JSON.parse(localStorage.prikeysidindex);
+						var len=data.length;
+						data[len]=[name,pri];
+						var keyidlen=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds().length;
+						var keyid=new Array();
+						for(ii=0;ii<keyidlen;++ii)
+							keyid[ii]=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds()[ii].toHex();
+						keysindex[len]=[keyid,len];
+						localStorage.prikeys=JSON.stringify(data);
+						localStorage.prikeysidindex=JSON.stringify(keysindex);
+					}
 				}else{
-					data=JSON.parse(localStorage.prikeys);
-					keysindex=JSON.parse(localStorage.prikeysidindex);
-					len=data.length;
-					data[len]=[name,pri];
-					keyidlen=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds().length;
-					keyid=new Array();
+					localStorage.prikeys=JSON.stringify(new Array([name,pri]));
+					var keyidlen=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds().length;
+					var keyid=new Array();
 					for(ii=0;ii<keyidlen;++ii)
-						keyid[ii]=(await openpgp.key.readArmored(pri(i))).keys[0].getKeyIds()[ii].toHex();
-					keysindex[len]=[keyid,len];
-					localStorage.prikeys=JSON.stringify(data);
-					localStorage.prikeysidindex=JSON.stringify(keysindex);
+						keyid[ii]=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds()[ii].toHex();
+					localStorage.prikeysidindex=JSON.stringify(new Array([keyid,0]));
 				}
-			}else{
-				localStorage.prikeys=JSON.stringify(new Array([name,pri]));
-				keyidlen=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds().length;
-				keyid=new Array();
-				for(ii=0;ii<keyidlen;++ii)
-					keyid[ii]=(await openpgp.key.readArmored(pri)).keys[0].getKeyIds()[ii].toHex();
-				localStorage.prikeysidindex=JSON.stringify(new Array([keyid,0]));
+			}catch(e){
+				alert("出错：\n调用函数：PriLocalSave("+name+','+pri+")\n错误代码："+e);
+				throw new Error(e);
 			}
 		}else{
 			window.alert("您的浏览器不支持此功能，请更新浏览器");
@@ -68,16 +82,16 @@ function  PriLocalSave(name,pri){
 function PriReadIndex(){
 	if((typeof(localStorage.prikeys)!=="undefined")||(typeof(sessionStorage.prikeys)!=="undefined")){
 		if(localStorage.prikeys.length!==0){
-			localdata=JSON.parse(localStorage.prikeys);
-			len=localdata.length;
-			res=new Array();
+			var localdata=JSON.parse(localStorage.prikeys);
+			var len=localdata.length;
+			var res=new Array();
 			for(var i=0;i<len;++i){
 				res[i]=[i,localdata[i][0]];
 			}
 		}
 		if((typeof(sessionStorage.prikeys)!=="undefined")&&(sessionStorage.prikeys.length!==0)){
-			sessdata=JSON.parse(sessionStorage.prikeys);
-			sesslen=sessdata.length;
+			var sessdata=JSON.parse(sessionStorage.prikeys);
+			var sesslen=sessdata.length;
 			for(var i=len;i<sesslen+len;++i){
 				res[i]=[i,sessdata[i-len][0]];
 			}
@@ -89,8 +103,8 @@ function PriReadIndex(){
 }
 
 function PriRead(id){
-	data=JSON.parse(localStorage.prikeys);
-	len=data.length;
+	var data=JSON.parse(localStorage.prikeys);
+	var len=data.length;
 	if(id<len){
 		return data[id][1];
 	}else{
@@ -99,8 +113,8 @@ function PriRead(id){
 }
 
 function PriReadName(id){
-	data=JSON.parse(localStorage.prikeys);
-	len=data.length;
+	var data=JSON.parse(localStorage.prikeys);
+	var len=data.length;
 	if(id<len){
 		return data[id][0];
 	}else{
@@ -110,9 +124,9 @@ function PriReadName(id){
 
 function PriDel(id){
 	if(window.confirm("确认删除此私钥？")){
-		data=JSON.parse(localStorage.prikeys);
-		keysindex=JSON.parse(localStorage.prikeysidindex);
-		len=data.length;
+		var data=JSON.parse(localStorage.prikeys);
+		var keysindex=JSON.parse(localStorage.prikeysidindex);
+		var len=data.length;
 		if(id<len){
 			data[id]=data[len-1];
 			keysindex[id]=keysindex[len-1];
@@ -121,9 +135,9 @@ function PriDel(id){
 			localStorage.prikeys=JSON.stringify(data);
 			localStorage.prikeysidindex=JSON.stringify(keysindex);
 		}else{
-			data=JSON.parse(sessionStorage.prikeys);
-			keysindex=JSON.parse(sessionStorage.prikeysidindex);
-			sesslen=data.length;
+			var data=JSON.parse(sessionStorage.prikeys);
+			var keysindex=JSON.parse(sessionStorage.prikeysidindex);
+			var sesslen=data.length;
 			data[id-len]=data[sesslen-1];
 			keysindex[id-len]=keysindex[sesslen-1];
 			data.pop();
@@ -149,26 +163,26 @@ function PriGenIndex(){
 		if(typeof(Storage)!=="undefined"){
 			try{
 				if(typeof(localStorage.prikeys)!=="undefined"){
-					data=JSON.parse(localStorage.prikeys);
-					len=data.length;	//获取持久存储的大小
-					Index=new Array();
-					for(i=0;i<len;++i){
-						keyidlen=(await openpgp.key.readArmored(PriRead(i))).keys[0].getKeyIds().length;
-						keyid=new Array();
-						for(ii=0;ii<keyidlen;++ii)
+					var data=JSON.parse(localStorage.prikeys);
+					var len=data.length;	//获取持久存储的大小
+					var Index=new Array();
+					for(var i=0;i<len;++i){
+						var keyidlen=(await openpgp.key.readArmored(PriRead(i))).keys[0].getKeyIds().length;
+						var keyid=new Array();
+						for(var ii=0;ii<keyidlen;++ii)
 							keyid[ii]=(await openpgp.key.readArmored(PriRead(i))).keys[0].getKeyIds()[ii].toHex();
 						Index[i]=[keyid,i];
 					}
 					localStorage.prikeysidindex=JSON.stringify(Index);
 				}
 				if(typeof(sessionStorage.prikeys)!=="undefined"){
-					Index1=new Array();
-					data=JSON.parse(sessionStorage.prikeys);
-					len1=data.length;
-					for(i=len;i<len1+len;++i){
-						keyidlen=(await openpgp.key.readArmored(PriRead(i))).keys[0].getKeyIds().length;
-						keyid=new Array();
-						for(ii=0;ii<keyidlen;++ii)
+					var Index1=new Array();
+					var data=JSON.parse(sessionStorage.prikeys);
+					var len1=data.length;
+					for(var i=len;i<len1+len;++i){
+						var keyidlen=(await openpgp.key.readArmored(PriRead(i))).keys[0].getKeyIds().length;
+						var keyid=new Array();
+						for(var ii=0;ii<keyidlen;++ii)
 							keyid[ii]=(await openpgp.key.readArmored(PriRead(i))).keys[0].getKeyIds()[ii].toHex();
 						Index1[i-len]=[keyid,i];
 					}
@@ -177,6 +191,7 @@ function PriGenIndex(){
 
 			}catch(e){
 				alert("出错：\n调用函数：PriGenIndex()\n错误代码："+e);
+				throw new Error(e);
 			}
 		}else{
 			window.alert("您的浏览器不支持此功能，请更新浏览器");
@@ -189,15 +204,15 @@ function PriSearchId(input){
 	if(typeof(localStorage.prikeysidindex)==="undefined"){
 		PriGenIndex();
 	}
-	data=JSON.parse(localStorage.prikeysidindex);
-	for(i=0;i<input.length;++i){
-		for(ii=0;ii<data.length;++ii){
+	var data=JSON.parse(localStorage.prikeysidindex);
+	for(var i=0;i<input.length;++i){
+		for(var ii=0;ii<data.length;++ii){
 			if(data[ii][0].includes(input[i]))
 				return data[ii][1];
 		}
 		if(typeof(sessionStorage.prikeysidindex)!=="undefined"){
 			data=JSON.parse(sessionStorage.prikeysidindex);
-			for(ii=0;i<data.length;++ii){
+			for(var ii=0;i<data.length;++ii){
 				if(data[ii][0].includes(input[i])){
 					return data[ii][1];
 				}
--- js/storepub.js
+++ js/storepub.js
@@ -2,29 +2,38 @@ function PubSave(name,pub) {
 	(async () => {
 		if(typeof(Storage)!=="undefined")
 		{
-			if(typeof(localStorage.pubkeys)!=="undefined"){
-				if(JSON.parse(localStorage.getItem('pubkeys')).includes([name,pub])){ //这一段判断是否重复的代码似乎没有正常工作。
-					window.alert("此公钥已保存");
+			try{
+				if(typeof(localStorage.pubkeys)!=="undefined"){
+					if(JSON.parse(localStorage.getItem('pubkeys')).includes([name,pub])){ //这一段判断是否重复的代码似乎没有正常工作。
+						window.alert("此公钥已保存");
+					}else{
+						if(typeof(localStorage.pubkeysidindex)==="undefined")
+							PubGenIndex();
+						var data=JSON.parse(localStorage.pubkeys);
+						var keysindex=JSON.parse(localStorage.pubkeysidindex);
+						var len=data.length;
+						data[len]=[name,pub];
+						var keyids=(await openpgp.key.readArmored(pub)).keys[0].getKeyIds();
+						var keyidlen=keyids.length;
+						var keyid=new Array();
+						for(ii=0;ii<keyidlen;++ii)
+							keyid[ii]=keyids[ii].toHex();
+						keysindex[len]=[keyid,len];
+						localStorage.pubkeys=JSON.stringify(data);
+						localStorage.pubkeysidindex=JSON.stringify(keysindex);
+					}
 				}else{
-					data=JSON.parse(localStorage.getItem('pubkeys'));
-					keysindex=JSON.parse(localStorage.pubkeysidindex);
-					len=data.length;
-					data[len]=[name,pub];
-					keyidlen=(await openpgp.key.readArmored(pub)).keys[0].getKeyIds().length;
-					keyid=new Array();
+					localStorage.pubkeys=JSON.stringify(new Array([name,pub]));
+					var keyids=(await openpgp.key.readArmored(pub)).keys[0].getKeyIds();
+					var keyidlen=keyids.length;
+					var keyid=new Array();
 					for(ii=0;ii<keyidlen;++ii)
-						keyid[ii]=(await openpgp.key.readArmored(pub)).keys[0].getKeyIds()[ii].toHex();
-					keysindex[len]=[keyid,len];
-					localStorage.pubkeys=JSON.stringify(data);
-					localStorage.pubkeysidindex=JSON.stringify(keysindex);
+						keyid[ii]=keyids[ii].toHex();
+					localStorage.pubkeysidindex=JSON.stringify(new Array([keyid,0]));
 				}
-			}else{
-				localStorage.pubkeys=JSON.stringify(new Array([name,pub]));
-				keyidlen=(await openpgp.key.readArmored(pub)).keys[0].getKeyIds().length;
-				keyid=new Array();
-				for(ii=0;ii<keyidlen;++ii)
-					keyid[ii]=(await openpgp.key.readArmored(pub)).keys[0].getKeyIds()[ii].toHex();
-				localStorage.pubkeysidindex=JSON.stringify(new Array([keyid,0]));
+			}catch(e){
+				alert("出错：\n调用函数：PubSave("+name+','+pub+")\n错误代码："+e);
+				throw new Error(e);
 			}
 		} else {
 			window.alert("您的浏览器不支持此功能，请更新浏览器");
@@ -39,9 +48,9 @@ function PubReadIndex(){
 	if(localStorage.pubkeys.length===0){
 		return [];
 	}
-	data=JSON.parse(localStorage.pubkeys);
-	len=data.length;
-	res=new Array();
+	var data=JSON.parse(localStorage.pubkeys);
+	var len=data.length;
+	var res=new Array();
 	for(var i=0;i<len;++i){
 		res[i]=[i,data[i][0]];
 	}
@@ -49,19 +58,29 @@ function PubReadIndex(){
 }
 
 function PubRead(id){
-	return JSON.parse(localStorage.pubkeys)[id][1];
+	try{
+		return JSON.parse(localStorage.pubkeys)[id][1];
+	}catch(e){
+		alert("出错：\n调用函数：PubRead("+id+")\n错误代码："+e);
+		throw new Error(e);
+	}
 }
 
 function PubReadName(id){
-	data=JSON.parse(localStorage.pubkeys);
-	return data[id][0];
+	try{
+		var data=JSON.parse(localStorage.pubkeys);
+		return data[id][0];
+	}catch(e){
+		alert("出错：\n调用函数：PubReadName("+id+")\n错误代码："+e);
+		throw new Error(e);
+	}
 }
 
 function PubDel(id){
 	if(window.confirm("确认删除此公钥？")){
-		data=JSON.parse(localStorage.pubkeys);
-		keysindex=JSON.parse(localStorage.pubkeysidindex);
-		len=data.length;
+		var data=JSON.parse(localStorage.pubkeys);
+		var keysindex=JSON.parse(localStorage.pubkeysidindex);
+		var len=data.length;
 		data[id]=data[len-1];
 		keysindex[id]=keysindex[len-1];
 		data.pop();
@@ -83,20 +102,23 @@ function PubGenIndex(){
 	(async () => {
 		if(typeof(Storage)!=="undefined"){
 			try{
-				data=JSON.parse(localStorage.pubkeys);
-				len=data.length;	//获取持久存储的大小
-				Index=new Array();
+				var len=PubReadIndex().length;	//获取持久存储的大小
+				var Index=new Array();
 				for(i=0;i<len;++i){
-					keyidlen=(await openpgp.key.readArmored(PubRead(i))).keys[0].getKeyIds().length;
-					keyid=new Array();
-					for(ii=0;ii<keyidlen;++ii)
-						keyid[ii]=(await openpgp.key.readArmored(PubRead(i))).keys[0].getKeyIds()[ii].toHex();
+					var key=PubRead(i);
+					var keyids=(await openpgp.key.readArmored(key)).keys[0].getKeyIds();
+					var keyidlen=keyids.length;
+					var keyid=new Array();
+					for(ii=0;ii<keyidlen;++ii){
+						keyid[ii]=keyids[ii].toHex();
+					}
 					Index[i]=[keyid,i];
 				}
 				localStorage.pubkeysidindex=JSON.stringify(Index);
 
 			}catch(e){
 				alert("出错：\n调用函数：PubGenIndex()\n错误代码："+e);
+				throw new Error(e);
 			}
 		}else{
 			window.alert("您的浏览器不支持此功能，请更新浏览器");
@@ -108,7 +130,7 @@ function PubGenIndex(){
 function PubSearchId(input){
 	if(typeof(localStorage.pubkeysidindex)==="undefined")
 		PubGenIndex();
-	data=JSON.parse(localStorage.pubkeysidindex);
+	var data=JSON.parse(localStorage.pubkeysidindex);
 	for(i=0;i<input.length;++i){
 		for(ii=0;ii<data.length;++ii){
 			if(data[ii][0].includes(input[i]))
