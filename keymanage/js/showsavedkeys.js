3b068b94a84c88e67f09ae2dfcd0786f792e20fc
chenlhlinux <chenlhlinux@outlook.com>
2020-05-08 17:00:32 +0800

    feat(查看已保存的密钥):添加对多用户id的支持
    添加了对多用户id的支持，添加了一个字符转换函数，用于把一些特殊字符转换为HTML转义符

--- keymanage/js/showsavedkeys.js
+++ keymanage/js/showsavedkeys.js
@@ -5,7 +5,10 @@ $(async function(){
 		var key;
 		for(var i=0;i<index.length;++i){
 			key=(await openpgp.key.readArmored(PubRead(index[i][0]))).keys[0];
-			msg=msg+"<tr><td>"+index[i][1]+"</td><td>"+key.users[0].userId.name+"</td><td>"+key.users[0].userId.email+"</td><td>"+key.getCreationTime().toString()+"</td><td>"+key.getKeyId().toHex()+"</td><td>"+key.getFingerprint()+"</td></tr>";
+			var userid="";
+			for(var ii=0;ii<key.getUserIds().length;++ii)
+				userid+=char_conv(key.getUserIds()[ii])+'<br>';
+			msg=msg+"<tr><td>"+index[i][1]+"</td><td>"+userid+"</td><td>"+key.getCreationTime().toString()+"</td><td>"+key.getKeyId().toHex()+"</td><td>"+key.getFingerprint()+"</td></tr>";
 		}
 	}else{
 		var msg="<tr><td colspan='6'>没有已保存的密钥</td></tr>";
@@ -17,7 +20,10 @@ $(async function(){
 		msg="";
 		for(var i=0;i<index.length;++i){
 			key=(await openpgp.key.readArmored(PriRead(index[i][0]))).keys[0];
-			msg=msg+"<tr><td>"+index[i][1]+"</td><td>"+key.users[0].userId.name+"</td><td>"+key.users[0].userId.email+"</td><td>"+key.getCreationTime().toString()+"</td><td>"+key.getKeyId().toHex()+"</td><td>"+key.getFingerprint()+"</td></tr>";
+			var userid="";
+			for(var ii=0;ii<key.getUserIds().length;++ii)
+				userid+=char_conv(key.getUserIds()[ii])+'<br>';
+			msg=msg+"<tr><td>"+index[i][1]+"</td><td>"+userid+"</td><td>"+key.getCreationTime().toString()+"</td><td>"+key.getKeyId().toHex()+"</td><td>"+key.getFingerprint()+"</td></tr>";
 		}
 	}else{
 		msg="<tr><td colspan='6'>没有已保存的密钥</td></tr>";
